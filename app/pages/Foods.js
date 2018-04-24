21e44d143e05b87ca5a1a4ccfe0c8cd3198fb2a1
Iker <tdtrinh.web@gmail.com>
2018-04-24 15:02:20 +0700

    update UI

--- app/pages/Foods.js
+++ app/pages/Foods.js
@@ -66,52 +66,42 @@ class Foods extends ReactQueryParams {
     const category = categories[parseInt(params.index)]
 
     return (
-      <div className='row' style={{margin: '0'}}>
-        <div id='main-container' className='main-container pull-right' >
-          <section id='our-menus'>
-            <div className='our-menu section-padding' style = {{paddingTop: '30px', paddingBottom: '30px'}}>
-              <div className='section-title-container'>
-                <h2 className='section-title'>{category ? category.name : ''}</h2>
+      <div className='content'>
+        <div className='container-fluid animated fadeIn'>
+          <div className='row'>
+            <div className='card'>
+              <div className='card-header' data-background-color='purple'>
+                <h4 className='title' style={style.header}>{category ? category.name : ''}</h4>
               </div>
-              {foods.map((item, index) => {
-                if (category && category.id === item.foodCategoryId) {
-                  const image = R.values(item.imageUrl)
+              <div className='card-content'style={{ width: '100%', float: 'left', padding: '40px 20px' }}>
+                {foods.map((item, index) => {
+                  if (category && category.id === item.foodCategoryId) {
+                    const image = R.values(item.imageUrl)
 
-                  return (
-                    <div className='col-md-4 col-sm-4 food-item' key={index}>
-                      <article className='menus-container wow fadeIn animated' data-wow-delay='0.1s'>
-                        <div>
-                          <img src={ image.length > 0 ? image[0] : '' } style={{ objectFit: 'contain', width: '100%', height: '200px' }}/>
-                        </div>
-                        <h4 className='item-title' style={style.name}>{item.name}</h4>
-                        <div className='item-entry'>
-                          <p style={style.description}> {item.description}</p>
-                        </div>
-                        <div className='text-center number-order'>
-                          <Link className='fa fa-2x fa-minus-circle' style={style.selectButton} onClick={e => { e.preventDefault(); this.decreaseFood(item.id, dispatch) }}/>
-                          <span>{selectedFood && selectedFood[item.id.toString()] ? selectedFood[item.id.toString()].quantity : 0 }</span>
-                          <Link className='fa fa-2x fa-plus-circle' style={style.selectButton} onClick={e => { e.preventDefault(); this.increaseFood(item.id, dispatch) }}/>
-                        </div>
-                      </article>
-                    </div>
-                  )
-                }
-              })}
+                    return (
+                      <div className='col-md-4 col-sm-4 food-item' key={index}>
+                        <article className='menus-container wow fadeIn animated' data-wow-delay='0.1s'>
+                          <div>
+                            <img src={ image.length > 0 ? image[0] : '' } style={{ objectFit: 'contain', width: '100%', height: '200px' }}/>
+                          </div>
+                          <h4 className='item-title' style={style.name}>{item.name}</h4>
+                          <div className='item-entry'>
+                            <p style={style.description}> {item.description}</p>
+                          </div>
+                          <div className='text-center number-order'>
+                            <Link className='fa fa-2x fa-minus-circle' style={style.selectButton} onClick={e => { e.preventDefault(); this.decreaseFood(item.id, dispatch) }}/>
+                            <span style={style.quantity}>{selectedFood && selectedFood[item.id.toString()] ? selectedFood[item.id.toString()].quantity : 0 }</span>
+                            <Link className='fa fa-2x fa-plus-circle' style={style.selectButton} onClick={e => { e.preventDefault(); this.increaseFood(item.id, dispatch) }}/>
+                          </div>
+                        </article>
+                      </div>
+                    )
+                  }
+                })}
+              </div>
             </div>
-          </section>
-        </div>
-
-        <div id='scroll-to-top' className='right-fix-btn'>
-          <div className='go-to-top'>
-            <i className='fa fa-angle-double-up'></i>
           </div>
         </div>
-
-        <footer id='footer-section' className='main-container pull-right'>
-          <div className='copyrights '>
-            &copy; <a href='#'>Foody</a>  2015 - All Rights Reserved <span className='v-line'></span> Developed by <a href='http://jeweltheme.com'>Jewel Theme</a>
-          </div>
-        </footer>
       </div>
     )
   }
@@ -131,6 +121,13 @@ export default R.pipe(
 const style = {
   name: {
     textAlign: 'center'
+  },
+  quantity: {
+    userSelect: 'none'
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: '25px'
   },
   description: {
     overflow: 'hidden',
